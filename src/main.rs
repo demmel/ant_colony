a7545ebe752b94b2d81ef85f519becc780f62f12
David Emmel <dgemmel2@gmail.com>
2024-09-26 10:32:11 -0700

    Log sim stats every second

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ mod food;
 mod nest;
 mod track;
 
+use std::time::Duration;
+
 use ant::{
     decay_satiation, deposit_food, eat_held_food, eat_nest_food, emit_ant_pheromones, pick_up_food,
     rotate_ants, setup_ant_rendering, spawn_ant, starve, update_ant_holding_food, walk_ants,
+    HeldFood, Satiation,
 };
 use assets::{Colors, Meshes};
 use bevy::{
@@ -17,11 +20,12 @@ use bevy::{
     prelude::*,
     render::camera::ScalingMode,
     sprite::MaterialMesh2dBundle,
+    time::common_conditions::on_real_timer,
     window::WindowMode::BorderlessFullscreen,
 };
 use config::{SimulationConfig, LAYER_DIRT, TICKS_PER_SECOND, TICK_RATE_MULTIPLIER};
 use food::{setup_food_rendering, spawn_random_food, update_food_size};
-use nest::{emit_nest_pheromones, setup_nest_rendering, spawn_ants_from_nest, spawn_nest};
+use nest::{emit_nest_pheromones, setup_nest_rendering, spawn_ants_from_nest, spawn_nest, Nest};
 use rand::prelude::*;
 use track::{
     decay_tracks, diffuse_tracks, setup_tracks, setup_tracks_renderin, update_tracks_image,
@@ -70,6 +74,7 @@ fn add_simulation(mut app: App, label: impl ScheduleLabel) -> App {
                 (rotate_ants, spawn_ants_from_nest),
             )
                 .chain(),
+            log_stats.run_if(on_real_timer(Duration::from_secs(1))),
         )
             .chain(),),
     );
@@ -172,4 +177,21 @@ fn exit(keys: Res<ButtonInput<KeyCode>>) {
     }
 
     std::process::exit(0);
+}
+
+fn log_stats(ants: Query<(&HeldFood, &Satiation), With<ant::Ant>>, nests: Query<&Nest>) {
+    let ant_count = ants.iter().count();
+    let total_held_food: f32 = ants.iter().map(|(held_food, _)| held_food.amount()).sum();
+    let average_held_food = total_held_food / ant_count as f32;
+    let total_satiation: f32 = ants.iter().map(|(_, satiation)| satiation.amount()).sum();
+    let average_satiation = total_satiation / ant_count as f32;
+    let nest_count = nests.iter().count();
+    let total_nest_food: f32 = nests.iter().map(|nest| nest.food).sum();
+    let average_nest_food = total_nest_food / nest_count as f32;
+
+    info!("ants: {ant_count}");
+    info!("average held food: {average_held_food}");
+    info!("average satiation: {average_satiation}");
+    info!("nests: {nest_count}");
+    info!("average nest food: {average_nest_food}");
 }
